cf00b5147b224fce0fe946d7026d5a7e19fd2d69
Irwindeep <b22ai022@iitj.ac.in>
2025-04-11 07:42:29 +0530

    Add postprocessing steps for image segmentation

--- cellularvision/__init__.py
+++ cellularvision/__init__.py
@@ -2,10 +2,12 @@ from . import dataset
 from . import metrics
 from . import functional
 from . import models
+from . import postprocessing
 
 __all__ = [
     "dataset",
     "functional",
     "metrics",
-    "models"
+    "models",
+    "postprocessing"
 ]
---
+++ cellularvision/postprocessing/utils/__init__.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+from numpy.typing import NDArray
+
+label_colors = np.array([
+    [255, 255, 255],
+    [0, 128, 255],
+    [255, 0, 255],
+    [0, 100, 0],
+    [208, 187, 148],
+    [27, 18, 18]
+])
+
+def get_segmentation_contours(
+    image: NDArray[np.float32],
+    segmentation_mask: NDArray[np.uint8]
+) -> NDArray[np.float32]:
+    labels = np.unique(segmentation_mask)
+    labels = labels[labels != 0]
+
+    for label in labels:
+        color = tuple(label_colors[label].tolist())
+        label_mask = np.uint8(segmentation_mask == label)
+
+        contours, _ = cv2.findContours(label_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image, contours, -1, color, thickness=2)
+
+    return image
