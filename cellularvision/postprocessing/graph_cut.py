a55662cf160543c6ac0c12e626f496499372534e
Irwindeep <b22ai022@iitj.ac.in>
2025-04-12 00:37:55 +0530

    Update spectral clustering input as segmentation output

--- cellularvision/postprocessing/graph_cut.py
+++ cellularvision/postprocessing/graph_cut.py
@@ -1,16 +1,20 @@
 import numpy as np
 
-from scipy.sparse import spmatrix # type: ignore
 from scipy.sparse.linalg import eigsh # type: ignore
 from sklearn.cluster import KMeans # type: ignore
+from .utils import graph_laplacian
 
 from typing import Optional
 from numpy.typing import NDArray
 
 def spectral_clustering(
-    L_sparse: spmatrix, num_clusters: int,
-    num_eigenvectors: Optional[int] = None
+    segmentation_output: NDArray[np.float32],
+    num_clusters: int, num_eigenvectors: Optional[int] = None,
+    gamma: float = 1.0, neighbor_distance: int = 1
 ) -> NDArray[np.int32]:
+    L_sparse = graph_laplacian(
+        segmentation_output, gamma, neighbor_distance
+    )
     if num_eigenvectors is None:
         num_eigenvectors = num_clusters + 1
     
